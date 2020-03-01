0fac1a348796e9d2a9d76608e0d976f3d28c45ec
Fahad Mahmood <fahadmahmood20@gmail.com>
2020-03-02 00:25:10 +0500

    - Assign Property Value

--- AMC/AMC2O/Infrastructure/AutomapperConfiguration/Configuration.cs
+++ AMC/AMC2O/Infrastructure/AutomapperConfiguration/Configuration.cs
@@ -23,25 +23,44 @@ namespace AMC2O.Infrastructure.AutomapperConfiguration
                 cfg.ForAllMaps((typeMap, mappingExpression) =>
                 {
                     mappingExpression.PreserveReferences();
+                    mappingExpression.MaxDepth(10);
                     Expression<Action<object, object, ResolutionContext>> beforeMapAction = (src, dest, context) => BeforeMapActionFunc(src, dest, context);
                     typeMap.AddBeforeMapAction(beforeMapAction);
-
-                    ////mappingExpression.ConstructUsing((s, d) =>
-                    //// {
-                    ////     ////d.InstanceCache.Add(d, s);
-                    ////     return d;
-                    //// });
                 });
                 cfg.AllowNullDestinationValues = true;
                 cfg.AllowNullCollections = true;
                 cfg.Advanced.AllowAdditiveTypeMapCreation = true;
             });
-            ////config.AssertConfigurationIsValid();
+            config.AssertConfigurationIsValid();
             ////config.CompileMappings();
             return config.CreateMapper();
         }
         public static void BeforeMapActionFunc(object source, object destination, ResolutionContext resolutionContext)
         {
+            ////resolutionContext.Items.Add(resolutionContext.InstanceCache.Count.ToString(), resolutionContext);
+            ////var ttttt = resolutionContext
+            AssignPropertyValue(destination, resolutionContext, resolutionContext.InstanceCache);
+        }
+
+        private static void AssignPropertyValue(object value, ResolutionContext context, Dictionary<ContextCacheKey, object> cache)
+        {
+            var key = new ContextCacheKey(value, value.GetType());
+            if (cache.ContainsKey(key))
+            {
+                var parent = cache[key];
+
+                var parentProperty = value.GetType().GetProperty(parent.GetType().Name, parent.GetType());
+
+                if (parentProperty != null && parentProperty.CanWrite)
+                {
+                    parentProperty.SetValue(value, parent, null);
+                }
+            }
+
+            //if (context != null)
+            //{
+            //    AssignPropertyValue(value, context, cache);
+            //}
         }
     }
 }
